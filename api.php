050bddf3639078a45a2d39ab7fdea1ae981a5a10
Codenuclei <160272247+Codenuclei@users.noreply.github.com>
2024-02-17 00:08:30 +0530

    Add files via upload

---
+++ api.php
@@ -0,0 +1,14 @@
+<?php
+
+// Read the JSON file
+$json = file_get_contents('quotes.json');
+$data = json_decode($json, true);
+
+// Get the success quotes array
+$quotes = $data['success_quotes'];
+
+// Generate a random index
+$index = array_rand($quotes);
+
+// Return the random quote as JSON with unescaped unicode characters
+echo json_encode(["quote" => $quotes[$index]], JSON_UNESCAPED_UNICODE);
