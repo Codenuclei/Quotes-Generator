f8effbf97e5ae8940d4cdd2a01e3cdd2450853ec
Codenuclei <160272247+Codenuclei@users.noreply.github.com>
2024-08-05 16:09:34 +0530

    added hello

--- api.php
+++ api.php
@@ -7,8 +7,8 @@ $data = json_decode($json, true);
 // Get the success quotes array
 $quotes = $data['success_quotes'];
 
-// Generate a random index
+// Generate a random index for array
 $index = array_rand($quotes);
-
+//hello
 // Return the random quote as JSON with unescaped unicode characters
 echo json_encode(["quote" => $quotes[$index]], JSON_UNESCAPED_UNICODE);
